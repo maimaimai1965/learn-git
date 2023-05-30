cd98032c034f016bc10a6775e985a8735523598f
Andrey Miroshnik <A.Miroshnik@telesens.ua>
2023-05-30 11:39:00 +0300

    UserB change 2222222

--- src/main/java/com/example/learndemo/LearnDemoApplication.java
+++ src/main/java/com/example/learndemo/LearnDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LearnDemoApplication {
 // 1111111
+// 2222222
     public static void main(String[] args) {
         SpringApplication.run(LearnDemoApplication.class, args);
     }

e75ca74be01ece78dc8a6daf1f5b7d80b752dad9
Andrey Miroshnik <A.Miroshnik@telesens.ua>
2023-05-30 12:10:39 +0300

    UserA change 3333333 in branch-in-github

--- src/main/java/com/example/learndemo/LearnDemoApplication.java
+++ src/main/java/com/example/learndemo/LearnDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearnDemoApplication {
 // 1111111
 // 2222222
+// 3333333
     public static void main(String[] args) {
         SpringApplication.run(LearnDemoApplication.class, args);
     }
